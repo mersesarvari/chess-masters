fc96b903b4e7960f51e89282ca05f4da2c64eedc
mersesarvari <merse.sarvari@gmail.com>
2024-10-03 08:31:43 +0200

    renamed chess to board

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,21 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        bot: resolve(__dirname, "public/bot.js"),
+        content: resolve(__dirname, "public/board.js"),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === "main" ? "[name].[hash].js" : "[name].js";
+        },
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });

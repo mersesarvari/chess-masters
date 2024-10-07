583f124b8e913819c33eeaf8edbbcd1bee6923f7
mersesarvari <merse.sarvari@gmail.com>
2024-10-07 17:52:22 +0200

    added stop on checkmate and database saving

--- public/boardNEW.js
+++ public/boardNEW.js
@@ -2,6 +2,7 @@ let mycolor = "w";
 let isActive = false;
 let intervalId = null;
 let moves = [];
+let isStopOnCheckmateActive = false;
 
 async function Start() {
   getColor();
@@ -21,10 +22,13 @@ async function Start() {
     if (mycolor === "w" && moves.length % 2 === 0) {
       return true;
     }
+    if (mycolor === "b" && moves.length % 2 === 1) {
+      return true;
+    }
     return false;
   }
   async function showMoves(moves) {
-    // Fetch POST request to the specified endpoint
+    // Make a POST request to the specified endpoint
     async function fetchFen() {
       try {
         const response = await fetch(
@@ -96,6 +100,7 @@ async function Start() {
   console.log("[ChessMaster]: Started");
   //Showing move if no move happened and it is my turn (Im white)
   if (myTurn(moves) === true) {
+    console.log("Show moves because this is my turn!");
     await showMoves(moves);
   }
   //Showing moves if a move happened and it is my turn!
@@ -118,20 +123,100 @@ function Stop() {
   console.log("[ChessMaster]: Stopped");
 }
 
-function stopOnCheckmate() {
-  const checkInterval = setInterval(() => {
-    const checkmateElement = document.querySelector(
-      "#game-over-modal > div > div:nth-child(2) > section > div.modal-game-over-bg"
-    );
+async function stopOnCheckmate() {
+  if (isStopOnCheckmateActive || !isActive) {
+    return;
+  } else {
+    isStopOnCheckmateActive = true;
+    isActive = false;
+    const checkInterval = setInterval(async () => {
+      // Get all the text content on the page
+      const pageText = document.body.innerText || document.body.textContent;
 
-    if (isActive && checkmateElement) {
-      console.log("Stopped on checkmate");
-      isActive = false;
-      clearArrows();
-      stopFENChangeCheck();
-      clearInterval(checkInterval);
-    }
-  }, 100);
+      // Check if the text contains keywords indicating game over or checkmate
+      const isGameOver = pageText.includes("Game Review");
+
+      // Check if game is over and if a winner is found
+      if (isGameOver) {
+        //Stopping from re-running
+        isActive = false;
+        clearInterval(checkInterval);
+        clearArrows();
+
+        const mewon = pageText.includes("You Won!");
+        const blackWon = pageText.includes("Black Won!");
+        const whiteWon = pageText.includes("White Won!");
+        const draw = pageText.includes("Draw");
+
+        let winColor = "w"; // Initialize winColor
+        if (mewon) {
+          console.log(`You won with ${mycolor}`);
+          winColor = mycolor;
+        } else if (blackWon || whiteWon) {
+          console.log(`Opponent won with ${mycolor === "w" ? "b" : "w"}`);
+          winColor = mycolor === "w" ? "b" : "w";
+        } else if (draw) {
+          winColor = "-";
+          console.log("Draw");
+        }
+
+        // Fetch email asynchronously
+        chrome.storage.local.get(["email"], async (result) => {
+          const email = result.email;
+
+          if (!email) {
+            // If no email found, stop:
+            return;
+          }
+
+          // Prepare the game object
+          const gameObject = {
+            moves: moves,
+            winColor: winColor,
+            myColor: mycolor,
+            email: email, // Use the email retrieved from storage
+          };
+
+          // Saving the game to the database
+          try {
+            const response = await fetch(
+              "https://chess-master-webpage.vercel.app/api/game",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(gameObject),
+              }
+            );
+
+            if (!response.ok) {
+              console.error("Cannot save the game to the database!");
+            } else {
+              console.log("Game saved to the database!");
+            }
+          } catch (error) {
+            console.error("Cannot save the game to the database!", error);
+          } finally {
+            Stop();
+            console.log("[BOARD.js]: Stopped on checkmate");
+          }
+        });
+      }
+    }, 1000); // Check every 1000 milliseconds (1 second)
+  }
+}
+
+function getWinner() {
+  // Select the element containing the header with the winner text
+  const winnerElement = document.querySelector(".header-title-component");
+
+  // If the element exists, retrieve and return the text content (e.g., "White Won")
+  if (winnerElement) {
+    return winnerElement.textContent.trim();
+  } else {
+    return "Winner not found";
+  }
 }
 function checkMoves() {
   // Select all div elements with class 'node'
@@ -276,18 +361,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === "start") {
     console.log("[BOARD:js]: Start command recieved");
-    Start();
-    stopOnCheckmate();
+    await Start();
+    await stopOnCheckmate();
   }
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === "stop") {
     console.log("[BOARD:js]: stop command recieved");
     Stop();
-    stopOnCheckmate();
   }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,21 +9,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        bot: resolve(__dirname, "public/bot.js"),
-        content: resolve(__dirname, "public/board.js"),
-      },
-      output: {
-        entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === "main" ? "[name].[hash].js" : "[name].js";
-        },
-      },
-    },
-    outDir: "dist",
-    emptyOutDir: true,
   },
 });

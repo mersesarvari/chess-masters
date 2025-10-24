a9e2048919a312f1810b1d6e73c3df18e6a7519b
Merse Sárvári <merse.sarvari@gmail.com>
2025-10-24 11:24:09 +0200

    updated manifest

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import {
 
 export default function App() {
   // VERSION
-  const version = "1.3.4";
+  const version = "1.3.5";
 
   const [versionOk, setVersionOk] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
